64c4bdc7391210965b2862808b0a3d5863f398b3
Lauren Chambers <lchambers@aclumorg>
2021-07-07 16:08:37 -0400

    Update email to be d4j

--- app/app.R
+++ app/app.R
@@ -314,7 +314,7 @@ ui <- fluidPage(theme = "bpd_covid19_app.css",
           a(href="https://www.data.aclum.org/",  target="_blank",
             img(src="D4J-logo.png", height="50px", 
                 style="display: inline; margin: 10px;"))),
-      p("Please contact lchambers@aclum.org with questions.", align="center", style="opacity: 0.6;")
+      p("Please contact data4justice@aclum.org with questions.", align="center", style="opacity: 0.6;")
   )
 )
 
