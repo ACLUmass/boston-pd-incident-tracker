05865f2fe6de48f80c5cfbb5b75a411f5f96f895
Lauren Chambers <lauren.m.chambers96@gmail.com>
2023-09-19 17:22:02 -0400

    Alert my berkeley email too? idk if this is even working

--- app/send_error_email.R
+++ app/send_error_email.R
@@ -16,11 +16,11 @@ gm_auth(email = "lauren.m.chambers96@gmail.com",
 send_email <- function() {
 
   gm_mime() %>%
-    gm_to("lchambers@aclum.org") %>%
+    gm_to("lauren@ischool.berkeley.edu; lchambers@aclum.org") %>%
     gm_from("lauren.m.chambers96@gmail.com") %>%
     gm_subject("[R Shiny Error] BPD Incident Tracker") %>%
-    gm_text_body(paste("An error has occured.", 
-                       "\n\n\n", 
+    gm_text_body(paste("An error has occured.",
+                       "\n\n\n",
                        "https://www.shinyapps.io/admin/#/dashboard")) %>%
     # send it
     gm_send_message()
